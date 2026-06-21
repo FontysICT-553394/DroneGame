39ec7843652edb33b259bca9f2850d24ec39ba3e
RyanJoeW <Ryanwoestenberg@hotmail.com>
2026-06-21 21:42:48 +0200

    smoll polish

--- Assets/Scripts/JetPack/Movement/JetPackMovement.cs
+++ Assets/Scripts/JetPack/Movement/JetPackMovement.cs
@@ -18,35 +18,29 @@ public class JetPackMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
-        // We doen gravity zelf, zodat het meer als Jetpack Joyride voelt
+        
         rb.useGravity = false;
     }
 
     void Update()
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
-        //moveInput.z = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
     {
-        // Standaard val je naar beneden
         verticalVelocity -= gravityForce * Time.fixedDeltaTime;
-
-        // Jetpack omhoog als je Space ingedrukt houdt
+        
         if (Input.GetKey(KeyCode.Space))
         {
             verticalVelocity += jetpackForce * Time.fixedDeltaTime;
-
-            // Directe start omhoog
+            
             if (verticalVelocity < 2f)
             {
                 verticalVelocity = 2f;
             }
         }
-
-        // Limieten zodat het controllable blijft
+        
         verticalVelocity = Mathf.Clamp(verticalVelocity, maxFallSpeed, maxRiseSpeed);
 
         Vector3 horizontalVelocity = new Vector3(moveInput.x, 0f, moveInput.z).normalized * moveSpeed;
